f67c11f7cd03b9fbcdc300cbf52ea9817ab473b9
OmarNour <omar_nour@hotmail.com>
2019-04-17 13:34:33 +0200

    620 fix

--- templates/D620.py
+++ templates/D620.py
@@ -48,7 +48,7 @@ def d620(source_output_path, source_name, Table_mapping,Column_mapping,Core_tabl
 
         sub="/* Target table:	"+target_table+"*/"+'\n'+"/* Table mapping:	"+table_maping_name +"*/"+'\n'+"/* Mapping group:	"+table_maping_row['Mapping group'] +"*/"
         inp_view_select_clause='SELECT '+'\n'+sub+TransformDDL.get_select_clause(target_table, Core_tables, table_maping_name, Column_mapping)
-        map_grp=' CAST(' +table_maping_row['Mapping group']+' AS VARCHAR(100)) AS  MAP_GROUP ,'
+        map_grp=' CAST(' +funcs.single_quotes(table_maping_row['Mapping group'])+' AS VARCHAR(100)) AS  MAP_GROUP ,'
         start_date='(SELECT Business_Date FROM GDEV1V_GCFR.GCFR_Process_Id'+'\n'+'   WHERE Process_Name = '+ "'"+process_name+ "'"+'\n'+') AS Start_Date,'
         end_date='DATE '+"'9999-12-31'"+' AS End_Date,'
         modification_type=''

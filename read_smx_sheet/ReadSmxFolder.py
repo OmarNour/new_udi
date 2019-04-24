324679caed7cf1e04b44886bc16236bbaa977a1e
OmarNour <omar_nour@hotmail.com>
2019-04-24 10:41:30 +0200

    enhancements

--- read_smx_sheet/ReadSmxFolder.py
+++ read_smx_sheet/ReadSmxFolder.py
@@ -27,6 +27,41 @@ class ReadSmxFolder:
         self.parallel_build_source_scripts = []
         self.parallel_save_sheet_data = []
 
+    def read_smx_folder(self):
+        print("Reading from: \t", pm.smx_path)
+        print("Output folder: \t", self.output_path)
+        print(pm.smx_ext + " files:")
+
+        read_smx_source_inputs = []
+        for smx in md.get_files_in_dir(pm.smx_path, pm.smx_ext):
+            self.count_smx = self.count_smx + 1
+            smx_file_name = os.path.splitext(smx)[0]
+            print("\t" + smx_file_name)
+            home_output_path = self.output_path + "/" + smx_file_name + "/"
+            smx_file_path = pm.smx_path + "/" + smx
+            read_smx_source_inputs.append([home_output_path,smx_file_path])
+
+            md.remove_folder(home_output_path)
+            md.create_folder(home_output_path)
+            gcfr.gcfr(home_output_path)
+
+            self.parallel_read_smx_sheets.append(delayed(self.read_smx_sheets)(smx_file_path))
+
+        if len(self.parallel_read_smx_sheets) > 0:
+            cpu_count = multiprocessing.cpu_count()
+            compute(*self.parallel_read_smx_sheets, num_workers=cpu_count)
+            with ProgressBar():
+                print("\nReading SMX Sheets...")
+                compute(*self.parallel_save_sheet_data, num_workers=cpu_count)
+
+        for i in read_smx_source_inputs:
+            i_home_output_path = i[0]
+            i_smx_file_path = i[1]
+            self.build_smx_source_scripts(i_home_output_path, i_smx_file_path)
+
+        funcs.wait_for_processes_to_finish(self.processes_numbers, self.processes_run_status, self.processes_names)
+        os.startfile(self.output_path)
+
     def read_smx_sheets(self, smx_file_path):
         try:
             teradata_sources_filter = [['Source type', ['TERADATA']]]
@@ -53,13 +88,11 @@ class ReadSmxFolder:
             self.parallel_save_sheet_data.append(delayed(funcs.save_sheet_data)(STG_tables, smx_file_path, self.output_path, 'STG tables'))
             self.parallel_save_sheet_data.append(delayed(funcs.save_sheet_data)(Table_mapping, smx_file_path, self.output_path, 'Table mapping'))
 
-            # self.parallel_read_smx_source.append(delayed(self.read_smx_source)(home_output_path, smx_file_path))
-
         except Exception as error:
             # print("0", error)
             pass
 
-    def read_smx_source(self, home_output_path, smx_file_path):
+    def build_smx_source_scripts(self, home_output_path, smx_file_path):
 
         module_path = os.path.dirname(sys.modules['__main__'].__file__)
         teradata_sources = funcs.get_sheet_data(smx_file_path, self.output_path, "System")
@@ -93,43 +126,6 @@ class ReadSmxFolder:
                 pass
                 # traceback.print_exc()
                 self.count_sources = self.count_sources - 1
-
-    def read_smx_folder(self):
-        print("Reading from: \t", pm.smx_path)
-        print("Output folder: \t", self.output_path)
-        print(pm.smx_ext + " files:")
-
-        read_smx_source_inputs = []
-        for smx in md.get_files_in_dir(pm.smx_path, pm.smx_ext):
-            self.count_smx = self.count_smx + 1
-            smx_file_name = os.path.splitext(smx)[0]
-            print("\t" + smx_file_name)
-            home_output_path = self.output_path + "/" + smx_file_name + "/"
-            smx_file_path = pm.smx_path + "/" + smx
-            read_smx_source_inputs.append([home_output_path,smx_file_path])
-
-            md.remove_folder(home_output_path)
-            md.create_folder(home_output_path)
-            gcfr.gcfr(home_output_path)
-
-            self.parallel_read_smx_sheets.append(delayed(self.read_smx_sheets)(smx_file_path))
-
-        if len(self.parallel_read_smx_sheets) > 0:
-            cpu_count = multiprocessing.cpu_count()
-            compute(*self.parallel_read_smx_sheets, num_workers=cpu_count)
-            with ProgressBar():
-                print("\nReading SMX Sheets...")
-                compute(*self.parallel_save_sheet_data, num_workers=cpu_count)
-
-        for i in read_smx_source_inputs:
-            i_home_output_path = i[0]
-            i_smx_file_path = i[1]
-            self.read_smx_source(i_home_output_path, i_smx_file_path)
-
-        # smx_files = " smx files" if self.count_smx > 1 else " smx file"
-        # print("\nStart generating scripts for " + str(self.count_sources) + " sources from " + str(self.count_smx) + smx_files)
-        funcs.wait_for_processes_to_finish(self.processes_numbers, self.processes_run_status, self.processes_names)
-        os.startfile(self.output_path)
 
 
 if __name__ == '__main__':

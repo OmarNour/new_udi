921472063dc32f883a829ec152ece324bddb17f3
OmarNour <omar_nour@hotmail.com>
2019-05-26 09:46:36 +0200

    enhancements

--- read_smx_sheet/templates/D000.py
+++ read_smx_sheet/templates/D000.py
@@ -33,7 +33,6 @@ def d000(cf, source_output_path, source_name, Table_mapping, STG_tables, BKEY):
 
                 process_name = "BK_" + str(Key_set_id) + "_" + Table_name + "_" + Column_name + "_" + str(Key_domain_ID)
 
-                f.write("delete from " + cf.GCFR_t + "." + cf.etl_process_table + " where process_name = '" + process_name + "';\n")
                 f.write("insert into " + cf.GCFR_t + "." + cf.etl_process_table + "(SOURCE_NAME, PROCESS_TYPE, PROCESS_NAME, BASE_TABLE, APPLY_TYPE, RECORD_ID)\n")
                 f.write("VALUES ('" + source_name + "', '" + prcess_type + "', '" + process_name + "', '" + target_table + "', '" + Historization_algorithm + "', NULL)" + ";\n")
                 f.write("\n")

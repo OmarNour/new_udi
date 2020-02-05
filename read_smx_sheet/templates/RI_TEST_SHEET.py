58f9b62f72abc087228332029e9f5f644ad37da9
dinaelhusseiny <dinahusseiny15@gmail.com>
2020-02-05 11:47:15 +0200

    -Fixes in testing scripts -Add join with cso in srci if the bkey domain id = 1 and domain name = bkey_1_prty -Make name of cso staging view

--- read_smx_sheet/templates/RI_TEST_SHEET.py
+++ read_smx_sheet/templates/RI_TEST_SHEET.py
@@ -15,14 +15,16 @@ def ri_check(cf, source_output_path, table_mapping, RI_relations):
             for ri_table_index,ri_table_row in RI_relations.iterrows():
                 RI_line = "---RI_Test_Case_" + str(count) + "---"
                 if ri_table_row['CHILD TABLE'] == table_name :
-                    call_line1 = "SELECT " + cf.base_DB + '.' + ri_table_row['CHILD TABLE'] + '.' + ri_table_row['CHILD COLUMN']
-                    call_line2 = " FROM " + ri_table_row['CHILD TABLE'] + " LEFT JOIN " + cf.base_DB + '.' + ri_table_row['PARENT TABLE']
-                    call_line3 = " ON " + ri_table_row['CHILD TABLE'] + '.' + ri_table_row['CHILD COLUMN']
+                    call_line1 = "SELECT DISTINCT" + cf.base_DB + '.' + ri_table_row['CHILD TABLE'] + '.' + ri_table_row['CHILD COLUMN']
+                    call_line2 = " FROM " + cf.base_DB + '.' + ri_table_row['CHILD TABLE'] + " CHILD_TABLE LEFT JOIN " + cf.base_DB + '.' + ri_table_row['PARENT TABLE'] + " PARENT_TABLE "
+                    call_line3 = " ON CHILD_TABLE." + ri_table_row['CHILD COLUMN']
                     call_line4 = " = " + ri_table_row['PARENT TABLE'] + '.' + ri_table_row['PARENT COLUMN']
-                    call_line5 = " WHERE " + cf.base_DB + '.' + ri_table_row['PARENT TABLE'] + '.' + ri_table_row['PARENT COLUMN'] + " IS NULL"
-                    call_line6 = " AND " + cf.base_DB + '.' + ri_table_row['CHILD TABLE'] + '.' + ri_table_row['CHILD COLUMN'] + " IS NOT NULL"
+                    call_line5 = " WHERE PARENT_TABLE" + ri_table_row['PARENT COLUMN'] + " IS NULL"
+                    call_line6 = " AND CHILD_TABLE." + ri_table_row['CHILD COLUMN'] + " IS NOT NULL"
+                    call_line7 = " AND CHILD_TABLE.END_TS IS NOT NULL"
+                    call_line7 = call_line7 + " AND PARENT_TABLE.END_TS IS NOT NULL;"
 
-                    call_exp = RI_line+"\n"+call_line1+'\n'+call_line2 +'\n'+ call_line3+call_line4+'\n'+call_line5+call_line6+'\n\n\n'
+                    call_exp = RI_line+"\n"+call_line1+'\n'+call_line2 +'\n'+ call_line3+call_line4+'\n'+call_line5+call_line6+'\n'+call_line7+'\n\n\n'
                     f.write(call_exp)
                     count = count + 1
 

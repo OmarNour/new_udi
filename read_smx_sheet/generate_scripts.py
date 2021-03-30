a59a462df9da2a6f7098e06b0b4bdf79f720691e
dinaelhusseiny <dinahusseiny15@gmail.com>
2021-03-30 14:53:52 +0200

    -Script for comparing staging counts

--- read_smx_sheet/generate_scripts.py
+++ read_smx_sheet/generate_scripts.py
@@ -10,7 +10,7 @@ from read_smx_sheet.templates import BMAP_DUP_CD_TEST_SHEET,BMAP_DUP_DESC_TEST_S
 from read_smx_sheet.templates import DATA_SRC_TEST_SHEET, BMAP_CHECK_TEST_SHEET , BMAP_UNMATCHED_TEST_SHEET
 from read_smx_sheet.templates import HIST_STRT_END_NULL_TEST_SHEET, HIST_DUP_TEST_SHEET ,HIST_STRT_GRT_END_TEST_SHEET,HIST_TIME_GAP_TEST_SHEET
 from read_smx_sheet.templates import HIST_STRT_NULL_TEST_SHEET, RI_TEST_SHEET, compare_testing_inputview
-from read_smx_sheet.templates import D410, D415, D003, D630, D420, D210, D608, D615, D000, gcfr, D620, D001, D600, D607, D002, D340,D215
+from read_smx_sheet.templates import D410, D415, D003, D630, D420, D210, D608, D615, D000, gcfr, D620, D001, D600, D607, D002, D340,D215,stgCounts
 from read_smx_sheet.parameters import parameters as pm
 import traceback
 import datetime as dt
@@ -208,6 +208,7 @@ class GenerateScripts:
                                 history_output_path_testing = os.path.join(output_path_testing, "HISTORY_Cases_scripts")
                                 ri_output_path_testing = os.path.join(output_path_testing, "RI_Cases_scripts")
                                 input_view_output_path_testing = os.path.join(output_path_testing, "Testing_input_views")
+                                compare_stg_counts_output_path_testing = os.path.join(output_path_testing,"compare_stg_counts")
 
                                 self.parallel_create_output_source_path.append(delayed(md.create_folder)(main_output_path))
 
@@ -260,6 +261,8 @@ class GenerateScripts:
                                     self.parallel_create_output_source_path.append(delayed(md.create_folder)(history_output_path_testing))
                                     self.parallel_create_output_source_path.append(delayed(md.create_folder)(ri_output_path_testing))
                                     self.parallel_create_output_source_path.append(delayed(md.create_folder)(input_view_output_path_testing))
+                                    self.parallel_create_output_source_path.append(delayed(md.create_folder)(compare_stg_counts_output_path_testing))
+
 
                                     self.parallel_templates.append(delayed(testing_script_01.source_testing_script)(self.cf, output_path_testing,source_name,core_Table_mapping,Column_mapping, STG_tables,BKEY))
                                     self.parallel_templates.append(delayed(testing_script_02.source_testing_script)(self.cf, output_path_testing,source_name,core_Table_mapping, Core_tables))
@@ -280,8 +283,8 @@ class GenerateScripts:
                                     self.parallel_templates.append(delayed(HIST_STRT_NULL_TEST_SHEET.hist_start_null_check)(self.cf, history_output_path_testing, core_Table_mapping, Core_tables))
                                     self.parallel_templates.append(delayed(RI_TEST_SHEET.ri_check)(self.cf, ri_output_path_testing, core_Table_mapping, RI_relations))
                                     self.parallel_templates.append(delayed(D620.d620)(self.cf, input_view_output_path_testing, core_Table_mapping, Column_mapping, Core_tables, Loading_Type,'TESTING'))
-                                    self.parallel_templates.append(delayed(compare_testing_inputview.compare_views_check)(self.cf, input_view_output_path_testing, core_Table_mapping,'FROM_TESTING_TO_UDI'))
-                                    self.parallel_templates.append(delayed(compare_testing_inputview.compare_views_check)(self.cf, input_view_output_path_testing, core_Table_mapping,'FROM_UDI_TO_TESTING'))
+                                    self.parallel_templates.append(delayed(compare_testing_inputview.compare_views_check)(self.cf, input_view_output_path_testing, core_Table_mapping,'FROM_TESTING_TO_UDI'))                                    #self.parallel_templates.append(delayed(compare_testing_inputview.compare_views_check)(self.cf, input_view_output_path_testing, core_Table_mapping,'FROM_UDI_TO_TESTING'))
+                                    self.parallel_templates.append(delayed(stgCounts.stgCounts)(self.cf, compare_stg_counts_output_path_testing,system_row,STG_tables))
 
                         except Exception as e_source:
                             # print(error)
---
+++ read_smx_sheet/templates/stgCounts.py
@@ -0,0 +1,44 @@
+from read_smx_sheet.app_Lib import functions as funcs
+from read_smx_sheet.Logging_Decorator import Logging_decorator
+from read_smx_sheet.parameters import parameters as pm
+
+
+@Logging_decorator
+def stgCounts(cf, source_output_path, System, STG_tables):
+    file_name = funcs.get_file_name(__file__)
+    f = funcs.WriteFile(source_output_path, file_name, "sql")
+    smx_path = cf.smx_path
+    template_path = cf.templates_path + "/" + pm.compareSTGcounts_template_filename
+    template_string = ""
+    try:
+        REJ_TABLE_NAME = System['Rejection Table Name']
+    except:
+        REJ_TABLE_NAME = ''
+    try:
+        REJ_TABLE_RULE = System['Rejection Table Rules']
+    except:
+        REJ_TABLE_RULE = ''
+    try:
+        source_DB = System['Source DB']
+    except:
+        source_DB = ''
+
+    try:
+        template_file = open(template_path, "r")
+    except:
+        template_file = open(smx_path, "r")
+
+    for i in template_file.readlines():
+        if i != "":
+            template_string = template_string + i
+    stg_table_names = funcs.get_stg_tables(STG_tables)
+    for stg_tables_df_index, stg_tables_df_row in stg_table_names[(stg_table_names['Table name'] != REJ_TABLE_NAME) & (stg_table_names['Table name'] != REJ_TABLE_RULE)].iterrows():
+        TABLE_NAME = stg_tables_df_row['Table name']
+        output_script = template_string.format(TABLE_NAME=TABLE_NAME,
+                                               STG_DATABASE=cf.T_STG,
+                                               WRK_DATABASE=cf.t_WRK,
+                                               source_DB=source_DB
+                                               )
+        output_script = output_script.upper() + '\n' + '\n' + '\n'
+        f.write(output_script.replace('Â', ' '))
+    f.close()

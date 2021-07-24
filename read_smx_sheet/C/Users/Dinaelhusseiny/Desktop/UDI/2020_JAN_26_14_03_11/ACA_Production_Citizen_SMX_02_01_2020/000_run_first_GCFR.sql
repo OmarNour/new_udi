6469bb8f3209a67921fddb6b3f5b3a76fdc038c9
dinaelhusseiny <dinahusseiny15@gmail.com>
2021-07-24 22:34:10 +0200

    -intial load

--- read_smx_sheet/C/Users/Dinaelhusseiny/Desktop/UDI/2020_JAN_26_14_03_11/ACA_Production_Citizen_SMX_02_01_2020/000_run_first_GCFR.sql
+++
@@ -1,9 +0,0 @@
-exec GPROD1M_GCFR.GCFR_Register_System(1, 'Economic', '',  'Economic');
-call GPROD1P_UT.GCFR_UT_Register_Stream(1, 1, 'Economic stream', cast('2019-01-01' as date));
-
-delete from GPROD1t_GCFR.PARAMETERS where PARAMETER_ID in (11, 7, 10);
-
-insert into GPROD1t_GCFR.PARAMETERS values (11, 'LRD T DB', 'GPROD1T_SRCI');
-insert into GPROD1t_GCFR.PARAMETERS values (7, 'INPUT V DB', 'GPROD1V_INP');
-insert into GPROD1t_GCFR.PARAMETERS values (10, 'BASE T DB', 'GPROD1T_BASE');
-

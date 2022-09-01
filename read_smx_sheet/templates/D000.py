0b9988f04702510e3c49c01f7eb45ea1b0e3a154
marcEssam48 <marcessam48_625@fci.helwan.edu.eg>
2022-09-01 10:52:59 +0200

    Updating the insert statements of D001 to insert the delta of data in SOURCE_TABLES_LKP

--- read_smx_sheet/templates/D000.py
+++ read_smx_sheet/templates/D000.py
@@ -6,7 +6,7 @@ from read_smx_sheet.Logging_Decorator import Logging_decorator
 def d000(cf, source_output_path, source_name, Table_mapping, STG_tables, BKEY):
     file_name = funcs.get_file_name(__file__)
     f = funcs.WriteFile(source_output_path, file_name, "sql")
-    # f.write("delete from " + cf.GCFR_t + "." + cf.etl_process_table + " where SOURCE_NAME = '" + source_name + "';\n\n")
+    f.write("--delete from " + cf.GCFR_t + "." + cf.etl_process_table + " where SOURCE_NAME = '" + source_name + "';\n\n")
     for table_maping_index, table_maping_row in Table_mapping.iterrows():
         prcess_type = "TXF"
         layer = str(table_maping_row['Layer'])

8fbc7747acc83eb2ab6ee1a4dfbb584897ed77e9
marcEssam48 <marcessam48_625@fci.helwan.edu.eg>
2023-02-02 10:25:37 +0200

    Adding not exists

--- read_smx_sheet/parameters/parameters.py
+++ read_smx_sheet/parameters/parameters.py
@@ -23,7 +23,10 @@ dataValidationAll_template_filename = "dataValidationALL.txt"
 sheets = [System_sht, Supplements_sht, Column_mapping_sht, BMAP_values_sht, BMAP_sht, BKEY_sht, STG_tables_sht, Table_mapping_sht, Core_tables_sht]
 
 # AppName_<Major>.<Minor>.<BuildNo>
-ver_no = "| Build #2.14.5"
+ver_no = "| Build #2.14.6"
+# ################################################################################################
+# What is new : #2.14.6 :
+# converting not in to not exists in 615 script
 # ################################################################################################
 # What is new : #2.14.5 :
 # Adding alias to the joins

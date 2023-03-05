8a634a2ea52eb92fb46971b2016761758b2d3f23
Omar Hosny <omarhosnyyy95@gmail.com>
2023-03-05 13:57:39 +0200

    add current_data, current_ts and current_user to D615

--- read_smx_sheet/templates/D615.py
+++ read_smx_sheet/templates/D615.py
@@ -10,7 +10,7 @@ def d615(cf, source_output_path, Core_tables,STG_tables):
     f = funcs.WriteFile(source_output_path, file_name, "sql")
     core_tables_df = funcs.get_core_tables(Core_tables)
     staging_tables_df = funcs.get_stg_tables(STG_tables)
-    insert_statement = f"INSERT INTO {cf.GCFR_t}.GCFR_Transform_KeyCol(OUT_DB_NAME, OUT_OBJECT_NAME, KEY_COLUMN)\n"
+    insert_statement = f"INSERT INTO {cf.GCFR_t}.GCFR_Transform_KeyCol(OUT_DB_NAME, OUT_OBJECT_NAME, KEY_COLUMN, UPDATE_DATE, UPDATE_USER, UPDATE_TS)\n"
     with_statement = "WITH\n"
     key_tables_core = ""
     all_keys_core = ""
@@ -28,7 +28,7 @@ def d615(cf, source_output_path, Core_tables,STG_tables):
                 Column_name = core_table_columns_row['Column name']
                 
 
-                key_tables_core += f"NEW_KEY{counter}\nAS(SELECT '{cf.base_DB}' OUT_DB_NAME\n,'{core_table_name}' (VARCHAR(130)) OUT_OBJECT_NAME\n,'{Column_name}' (VARCHAR(130)) KEY_COLUMN\n)\n,"
+                key_tables_core += f"NEW_KEY{counter}\nAS(SELECT '{cf.base_DB}' OUT_DB_NAME\n,'{core_table_name}' (VARCHAR(130)) OUT_OBJECT_NAME\n,'{Column_name}' (VARCHAR(130)) KEY_COLUMN, CURRENT_DATE UPDATE_DATE, CURRENT_USER UPDATE_USER, CURRENT_TIMESTAMP UPDATE_TS\n)\n,"
                 all_keys_core += f"SELECT * FROM NEW_KEY{counter} K{counter} WHERE NOT EXISTS(SELECT * FROM {cf.GCFR_t}.GCFR_TRANSFORM_KEYCOL K WHERE K.OUT_DB_NAME=K{counter}.OUT_DB_NAME AND K.OUT_OBJECT_NAME=K{counter}.OUT_OBJECT_NAME) \nUNION\n"
                 counter+=1
                 
@@ -40,7 +40,7 @@ def d615(cf, source_output_path, Core_tables,STG_tables):
         for column in stage_columns:
             
 
-            key_tables_stg += f"NEW_KEY{counter}\nAS(SELECT '{cf.T_STG}' OUT_DB_NAME\n,'{stage_table}' (VARCHAR(130)) OUT_OBJECT_NAME\n,'{column}' (VARCHAR(130)) KEY_COLUMN\n)\n,"
+            key_tables_stg += f"NEW_KEY{counter}\nAS(SELECT '{cf.T_STG}' OUT_DB_NAME\n,'{stage_table}' (VARCHAR(130)) OUT_OBJECT_NAME\n,'{column}' (VARCHAR(130)) KEY_COLUMN, CURRENT_DATE UPDATE_DATE, CURRENT_USER UPDATE_USER, CURRENT_TIMESTAMP UPDATE_TS\n)\n,"
             all_keys_stg += f"SELECT * FROM NEW_KEY{counter} K{counter} WHERE NOT EXISTS(SELECT * FROM {cf.GCFR_t}.GCFR_TRANSFORM_KEYCOL K WHERE K.OUT_DB_NAME=K{counter}.OUT_DB_NAME AND K.OUT_OBJECT_NAME=K{counter}.OUT_OBJECT_NAME) \nUNION\n"
             counter+=1
 
@@ -54,8 +54,8 @@ def d615(cf, source_output_path, Core_tables,STG_tables):
 
 
     # build scripts
-    core_script = insert_statement + with_statement + key_tables_core + "ALL_KEYS AS\n(" + all_keys_core + ")\n\nSELECT OUT_DB_NAME,OUT_OBJECT_NAME, KEY_COLUMN FROM ALL_KEYS;\n\n" 
-    stage_script = insert_statement + with_statement + key_tables_stg + "ALL_KEYS AS\n(" + all_keys_stg + ")\n\nSELECT OUT_DB_NAME,OUT_OBJECT_NAME, KEY_COLUMN FROM ALL_KEYS;" 
+    core_script = insert_statement + with_statement + key_tables_core + "ALL_KEYS AS\n(" + all_keys_core + ")\n\nSELECT OUT_DB_NAME,OUT_OBJECT_NAME, KEY_COLUMN, UPDATE_DATE, UPDATE_USER, UPDATE_TS FROM ALL_KEYS;\n\n" 
+    stage_script = insert_statement + with_statement + key_tables_stg + "ALL_KEYS AS\n(" + all_keys_stg + ")\n\nSELECT OUT_DB_NAME,OUT_OBJECT_NAME, KEY_COLUMN, UPDATE_DATE, UPDATE_USER, UPDATE_TS FROM ALL_KEYS;" 
     
     # write to file
     f.write(core_script)

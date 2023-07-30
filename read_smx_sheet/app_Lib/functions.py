de84ef90bd243bb5fa4d5617a74971d203cf2561
Hosny <omar.hosny@teradata.com>
2023-07-30 12:00:03 +0300

    cleaning config file

--- read_smx_sheet/app_Lib/functions.py
+++ read_smx_sheet/app_Lib/functions.py
@@ -1,19 +1,18 @@
+import datetime as dt
 import os
-import sys
 
-# sys.path.append(os.getcwd())
-import numpy as np
-import pandas as pd
+import app_Lib.manage_directories as md
 # import pyarrow.parquet as pq
 # import pyarrow as pa
 # from pyarrow.formatting import *
 import dask.dataframe as dd
-import app_Lib.manage_directories as md
+# sys.path.append(os.getcwd())
+import numpy as np
+import pandas as pd
+import psutil
 # from read_smx_sheet.app_Lib import manage_directories as md
 # from read_smx_sheet.parameters import parameters as pm
 from parameters import parameters as pm
-import datetime as dt
-import psutil
 
 
 def read_excel(file_path, sheet_name, filter=None, reserved_words_validation=None, nan_to_empty=True):
